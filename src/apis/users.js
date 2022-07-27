cb9843350c5fcdd8bace597429f1dd5d64a20d09
eddie wang <wang8119@gmail.com>
2022-07-28 02:13:56 +0800

    大修補整合

--- src/apis/users.js
+++ src/apis/users.js
@@ -1,13 +1,19 @@
-import { apiHelper } from './../utils/helpers'
+import { apiHelper } from "./../utils/helpers";
 
 export default {
   getCurrentUser() {
-    return apiHelper.get('/get_current_user')
+    return apiHelper.get("/get_current_user");
   },
   get({ userId }) {
-    return apiHelper.get(`/users/${userId}`)
+    return apiHelper.get(`/users/${userId}`);
   },
   getTopUsers() {
-    return apiHelper.get('/users/top')
-  }
-}
+    return apiHelper.get("/users/top");
+  },
+  addFollowing({ userId }) {
+    return apiHelper.post("/followships", { id: userId });
+  },
+  removeFollowing({ userId }) {
+    return apiHelper.delete(`/followships/${userId}`);
+  },
+};
--- src/components/RecommendColumn.vue
+++ src/components/RecommendColumn.vue
@@ -8,14 +8,15 @@
       />
 
       <div>
-        <a href="#" class="user-name">{{recommendUser.name}}</a>
-        <h4 class="user-acount"><span>@</span>{{recommendUser.account}}</h4>
+        <a href="#" class="user-name">{{ recommendUser.name }}</a>
+        <h4 class="user-acount"><span>@</span>{{ recommendUser.account }}</h4>
       </div>
 
       <button
         v-if="recommendUser.isFollowed"
         class="follow-btn empty-btn-style"
         type="submit"
+        @click.stop.prevent="removeFollowing(recommendUser.id)"
       >
         正在跟隨
       </button>
@@ -24,6 +25,7 @@
         v-else
         class="follow-btn empty-btn-style"
         type="submit"
+        @click.stop.prevent="addFollowing(recommendUser.id)"
       >
         跟隨
       </button>
@@ -32,19 +34,70 @@
 </template>
 
 <script>
+import { Toast } from "./../utils/helpers";
+import usersAPI from "./../apis/users";
+
 export default {
-  name: 'RecommendColumn',
+  name: "RecommendColumn",
   props: {
     initialRecommenduser: {
       type: Object,
-      required: true
-    }
+      required: true,
+    },
   },
-  data () {
+  data() {
     return {
       recommendUser: this.initialRecommenduser,
-      isProcessing: false
-    }
+      isProcessing: false,
+    };
   },
-}
+  methods: {
+    async addFollowing(userId) {
+      try {
+        this.isProcessing = true;
+        const { data } = await usersAPI.addFollowing({ userId });
+        console.log("following users=", data);
+        // if (data.status !== 'success') {
+        //   throw new Error(data.message)
+        // }
+
+        this.recommendUser = {
+          ...this.recommendUser,
+          isFollowed: true,
+        };
+        this.isProcessing = false;
+      } catch (error) {
+        console.error(error.message);
+        this.isProcessing = false;
+        Toast.fire({
+          icon: "error",
+          title: "目前無法跟隨使用者，請稍後再試",
+        });
+      }
+    },
+    async removeFollowing(userId) {
+      try {
+        this.isProcessing = true;
+        const { data } = await usersAPI.removeFollowing({ userId });
+        console.log("following users=", data);
+        // if (data.status === 'error') {
+        //   throw new Error(data.message)
+        // }
+
+        this.recommendUser = {
+          ...this.recommendUser,
+          isFollowed: false,
+        };
+        this.isProcessing = false;
+      } catch (error) {
+        console.error(error.message);
+        this.isProcessing = false;
+        Toast.fire({
+          icon: "error",
+          title: "目前無法取消跟隨使用者，請稍後再試",
+        });
+      }
+    },
+  },
+};
 </script>
