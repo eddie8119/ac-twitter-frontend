f5003b8094cd49542a2c4bcd4ce69b34880c703c
eddie wang <wang8119@gmail.com>
2022-08-01 17:58:50 +0800

    E05 更新 推薦跟隨區的 正在更隨按鈕ui

---
+++ src/views/AdminMain.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="d-flex justify-content-center">
+    <NavBar />
+    <div class="main-wrapper">
+      <NavpillHeaderMain />      
+    </div>   
+  </div>
+</template>
+
+<script>
+import NavBar from '../components/NavBar.vue';
+import NavpillHeaderMain from '../components/NavpillHeaderMain.vue';
+
+
+
+export default {
+	name: 'MainPage',
+	components: {
+		NavBar,		
+		NavpillHeaderMain,		
+	},
+	data() {
+		return {
+			
+		};
+	},
+	created() {
+		
+	},
+	methods: {
+		
+	},
+};
+</script>
