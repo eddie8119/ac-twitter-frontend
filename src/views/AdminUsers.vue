1c1c278d9f0cce48833a33d3824102b6ad51c407
eddie wang <wang8119@gmail.com>
2022-08-03 20:39:20 +0800

    E06 navbar css重構 + adminusercard ui元件

---
+++ src/views/AdminUsers.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="d-flex justify-content-center"> 
+    <NavBar />
+    <div class="admin-main-wrapper ml-6">
+      <NavpillHeaderMain />
+      <div class="y-scroll scrollbar">
+        <AdminUserCard />
+      </div>
+    </div>   
+  </div>
+</template>
+
+<script>
+import NavBar from '../components/NavBar.vue';
+import NavpillHeaderMain from '../components/NavpillHeaderMain.vue';
+import AdminUserCard from '../components/AdminUserCard.vue';
+
+
+export default {
+	name: 'AdminMain',
+	components: {
+		NavBar,		
+		NavpillHeaderMain,
+    AdminUserCard,		
+	},	
+	data() {
+		return {
+					
+		};
+	},
+};
+</script>
